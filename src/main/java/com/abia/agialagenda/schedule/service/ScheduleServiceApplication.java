b980ed204391ca973e97f150e8927dba11c6598c
Jeferson Silva <jefersoncj@outlook.com>
2025-12-06 16:22:21 -0300

    New project files

---
+++ src/main/java/com/abia/agialagenda/schedule/service/ScheduleServiceApplication.java
@@ -0,0 +1,13 @@
+package com.abia.agialagenda.schedule.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleServiceApplication.class, args);
+	}
+
+}
